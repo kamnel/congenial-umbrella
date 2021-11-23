70d74437aec9f423b02105adb7d549d7bf8a6de7
Kamal Nellipudi <kamal.nellipudi@vista.co>
2021-11-23 15:07:17 +1300

    Sample library

---
+++ PrimeGenerator/CpuUtils.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vista.Sample.Utils.Library
+{
+    public class CpuUtils
+    {
+        public static IEnumerable<int> GetRandomNumbers(int num)
+        {
+            // Source must be array or IList.
+            var source = Enumerable.Range(0, num).ToArray();
+
+            // Partition the entire source array.
+            var rangePartitioner = Partitioner.Create(0, source.Length);
+            var rand = new Random();
+
+            int[] results = new int[source.Length];
+
+            // Loop over the partitions in parallel.
+            Parallel.ForEach(rangePartitioner, (range, loopState) =>
+            {
+                // Loop over each range element without a delegate invocation.
+                for (int i = range.Item1; i < range.Item2; i++)
+                {
+                    results[i] = rand.Next();
+                }
+            });
+            return results;
+
+        }
+    }
+}
---
+++ PrimeGenerator/PrimeGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+
+namespace Vista.Sample.Utils.Library
+{
+    public class PrimeGenerator
+    {
+        public static IEnumerable GetPrimeNumbers(int num)
+        {
+            return Enumerable.Range(1, Convert.ToInt32(Math.Floor(Math.Sqrt(num))))
+                .Aggregate(Enumerable.Range(1, num).ToList(),
+                (result, index) =>
+                    {
+                        result.RemoveAll(i => i > result[index] && i%result[index] == 0);
+                        return result;
+                    }
+                );
+        }
+    }
+
+}
